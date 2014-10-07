b6b6d67cf95c415a4115a1c9257557e8ebaf7417
Tim Johnsen <tim@flipboard.com>
2014-10-07 16:51:48 -0700

    Hello World!

---
+++ RebaseSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RebaseSample
+//
+//  Created by Tim Johnsen on 10/7/14.
+//  Copyright (c) 2014 flipboard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
