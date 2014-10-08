be8076b5acb99e180d8325bb111f99f2c4e12f3e
Tim Johnsen <tim@flipboard.com>
2014-10-07 17:18:29 -0700

    Add method that causes the background views to "pulse".

--- RebaseSample/ViewController.m
+++ RebaseSample/ViewController.m
@@ -29,5 +29,25 @@
     [self transition];
 }
 
+- (void)transition
+{
+    UIColor *randomColor = [UIColor colorWithRed:arc4random_uniform(1000) / 1000.0 green:arc4random_uniform(1000) / 1000.0 blue:arc4random_uniform(1000) / 1000.0 alpha:1.0];
+    if (self.pulsingBackgroundView2.alpha == 0.0) {
+        self.pulsingBackgroundView2.backgroundColor = randomColor;
+        [UIView animateWithDuration:1.0 animations:^{
+            self.pulsingBackgroundView2.alpha = 1.0;
+        }];
+    } else {
+        self.pulsingBackgroundView1.backgroundColor = randomColor;
+        [UIView animateWithDuration:1.0 animations:^{
+            self.pulsingBackgroundView2.alpha = 0.0;
+        }];
+    }
+    
+    __weak ViewController *weakSelf = self;
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [weakSelf transition];
+    });
+}
 
 @end
