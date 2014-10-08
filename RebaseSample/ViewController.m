d901a2dcc7cfd6565f80258f4f060d3b3e196b98
Tim Johnsen <tim@flipboard.com>
2014-10-07 17:18:06 -0700

    Add pulsing background views.

--- RebaseSample/ViewController.m
+++ RebaseSample/ViewController.m
@@ -10,12 +10,23 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) UIView *pulsingBackgroundView1;
+@property (nonatomic, strong) UIView *pulsingBackgroundView2;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.pulsingBackgroundView1 = [[UIView alloc] initWithFrame:self.view.bounds];
+    [self.view addSubview:self.pulsingBackgroundView1];
+    
+    self.pulsingBackgroundView2 = [[UIView alloc] initWithFrame:self.view.bounds];
+    self.pulsingBackgroundView2.alpha = 0.0;
+    [self.view addSubview:self.pulsingBackgroundView2];
+    
+    [self transition];
 }
 
 
